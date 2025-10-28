7f4bb28d6e52768a3e828c6fc3070690ef7110d0
yasnoBruh <nikita.zavodchikov@yandex.ru>
2025-10-28 16:45:39 +0500

    Созданная библиотека классов+Snakes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,10 +6,12 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
-        {
-        }
+        public static List<Leaders> Leaders = new List<Leaders>();
+        public static List<ViewModelUserSettings> remoteIPAddress = new List<ViewModelUserSettings>();
+        public static List<ViewModelGames> viewModelGames = new List<ViewModelGames>();
+        private static int localPort = 5001;
+        public static int MaxSpreed = 15;
     }
 }
